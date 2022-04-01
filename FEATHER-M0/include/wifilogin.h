17e6b2c334f1ff93c273eda021f45369b8d72bb2
Moon Disco <37159553+moondisco@users.noreply.github.com>
2022-04-01 13:47:52 -0400

    Update wifilogin.h

--- FEATHER-M0/include/wifilogin.h
+++ FEATHER-M0/include/wifilogin.h
@@ -1,12 +1,6 @@
 #ifndef WIFILOGIN_H
 # define WIFILOGIN_H
 
-// #define SECRET_SSID "ETnet-5ghz" //Person al Wifi for testing without Pi.
-// #define SECRET_PASS "ETfiwi2021" //Person al Wifi for testing without Pi.
-
-// #define SECRET_SSID "EVOLVE Music" //Person al Wifi for testing without Pi.
-// #define SECRET_PASS "EvolveStudios" //Person al Wifi for testing without Pi.
-
 #define SECRET_SSID "RPiHotspot" //Pi RPiHotspot for deployment in field.
 #define SECRET_PASS "EvolveSensors" //Pi RPiHotspot for deployment in field.
 
